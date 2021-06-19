5570eee6f08c5799ebab707dfd1525ffb9e20cd1
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2021-06-19 21:23:00 +0530

    input field wiring done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  // hooks
+  const [userInput, setUserInput] = useState("");
+  // functions
+  const inputChangeHandler = (event) => {
+    setUserInput(event.target.value);
+  };
+  // main rendrer
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <h1>
+        {" "}
+        <span role="img" aria-label="burger-emoji">
+          🍔
+        </span>{" "}
+        Food & drink emoji interpreter
+      </h1>
+      <input onChange={inputChangeHandler} type="text" />
+      <p>{userInput}</p>
     </div>
   );
 }
